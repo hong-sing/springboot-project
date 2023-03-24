1cb8ccb846211fe05d1084c6dd183731c4b2af23
KHS <ewoking777@gmail.com>
2023-03-24 19:31:43 +0900

    재설정

--- src/main/java/com/ewok/springbootproject/web/IndexController.java
+++ src/main/java/com/ewok/springbootproject/web/IndexController.java
@@ -11,4 +11,9 @@ public class IndexController {
     public String index(Model model) {
         return "index";
     }
+
+    @GetMapping("/search")
+    public String search(Model model) {
+        return "/meme/post";
+    }
 }

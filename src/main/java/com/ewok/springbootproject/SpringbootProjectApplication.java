19c3943d66939ecfc3118c01101f1b7c68d40b8c
KHS <ewoking777@gmail.com>
2023-04-05 11:37:36 +0900

    feat : 배포 자동화 구성

--- src/main/java/com/ewok/springbootproject/SpringbootProjectApplication.java
+++ src/main/java/com/ewok/springbootproject/SpringbootProjectApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-@EnableJpaAuditing
+//@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(SpringbootProjectApplication.class, args);
+		SpringApplication application = new SpringApplication(SpringbootProjectApplication.class);
+		application.addListeners(new ApplicationPidFileWriter());
+		application.run(args);
 	}
 
 }

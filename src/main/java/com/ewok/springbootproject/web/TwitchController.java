46c64b45245d00c0fa783365f2be5b06ed00fecb
KHS <ewoking777@gmail.com>
2023-04-06 00:31:13 +0900

    chore : 불필요 코드 정리

--- src/main/java/com/ewok/springbootproject/web/TwitchController.java
+++ src/main/java/com/ewok/springbootproject/web/TwitchController.java
@@ -30,7 +30,6 @@ public class TwitchController {
 
     @GetMapping("/search/{streamer}")
     public String search(Model model, @PathVariable String streamer, @LoginUser SessionUser user, @PageableDefault(size = 15) Pageable pageable, @RequestParam(required = false, defaultValue = "0", value = "page") int pageNo) {
-        System.out.println(pageNo);
         if (user != null) {
             model.addAttribute("userName", user.getName());
         }
@@ -40,17 +39,6 @@ public class TwitchController {
 
         return "meme/post";
     }
-
-//    @GetMapping("/search/{streamer}")
-//    public String search(Model model, @PathVariable String streamer, @LoginUser SessionUser user, @PageableDefault(size = 15) Pageable pageable) {
-//        if (user != null) {
-//            model.addAttribute("userName", user.getName());
-//        }
-//        model.addAttribute("streamer", twitchService.getStreamerInfo(streamer));
-//        model.addAttribute("posts", postsService.findByLoginGoodDesc2(streamer, pageable).getBody());
-//
-//        return "meme/post";
-//    }
 
     @GetMapping("/post/detail/{id}")
     public String detail(Model model, @PathVariable Long id, @LoginUser SessionUser user) {

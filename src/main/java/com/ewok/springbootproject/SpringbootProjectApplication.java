6b9dd26d89b789984f0e01979a7be640f53301af
KHS <ewoking777@gmail.com>
2023-04-07 10:31:29 +0900

    fix : Application 클래스 EnableJpaAuditing 주석해제 created_date가 기록이 안되어 수정

--- src/main/java/com/ewok/springbootproject/SpringbootProjectApplication.java
+++ src/main/java/com/ewok/springbootproject/SpringbootProjectApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.ApplicationPidFileWriter;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-//@EnableJpaAuditing
+@EnableJpaAuditing
 @SpringBootApplication
 public class SpringbootProjectApplication {
 
